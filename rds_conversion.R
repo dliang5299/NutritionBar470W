233a559d022f0731b3c993fd1578091c07deb1c7
Deric Liang <33292727+dliang5299@users.noreply.github.com>
2021-03-18 14:02:02 -0400

    clean code, fix residual plots

--- rds_conversion.R
+++ rds_conversion.R
@@ -4,4 +4,7 @@ library(data.table)
 
 # read data and save as RDS
 nb <- fread("Data/NutritionBarData.csv")
-saveRDS(nb, file = "Data/nb.rds")
+nb1 <- nb %>%
+  mutate(Minutes = as.factor(Minutes),
+         Participant = as.factor(Participant))
+saveRDS(nb1, file = "Data/nb.rds")
