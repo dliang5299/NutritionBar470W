5dceeff885419e52855aacbac3b9a918063407ea
Deric Liang <33292727+dliang5299@users.noreply.github.com>
2021-03-22 15:28:26 -0400

    start EDA work

---
+++ eda_tukey.R
@@ -0,0 +1,36 @@
+# libraries
+library(tidyverse)
+
+# visual options
+theme_set(theme_bw())
+
+# load data
+nb <- readRDS("Data/nb.rds")
+
+# gender diff. in glucose
+ggplot(nb, aes(x = Gender, y = Glucose)) +
+  geom_boxplot()
+  
+# brand diff. in glucose
+ggplot(nb, aes(x = NutritionBar, y = Glucose)) +
+  geom_boxplot()
+
+# brand stability
+ggplot(nb, aes(x = NutritionBar, y = Glucose, fill = Minutes)) +
+  geom_boxplot()
+
+# minute diff. in glucose
+ggplot(nb, aes(x = Minutes, y = Glucose)) +
+  geom_boxplot()
+
+# 75 min. glucose
+nb75 <-
+  nb %>%
+  filter(Minutes == "75")
+ggplot(nb75, aes(x = NutritionBar, y = Glucose)) +
+  geom_boxplot() +
+  ylab("Glucose at 75 Minutes")
+
+# tukey plots
+
+
